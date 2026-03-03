981e211adde281206698fa389083f94904e36187
Prashant Upadhyay <prashantupadhyay412@gmail.com>
2026-03-03 19:18:07 +0530

    Added script for main page

--- script.js
+++ script.js
@@ -2,21 +2,27 @@ let boxes = document.querySelectorAll(".box");
 let reset = document.querySelector(".reset");
 let newgame=document.querySelector(".NewGame");
 let msgcontainer=document.querySelector(".msgcontainer");
+//index.html
 let msg=document.getElementById("msg");
-// let x=document.querySelectorAll(".player");
-// let start=document.getElementById("start");
-// let player1,player2;
-// start.addEventListener("click",function(){
-//     player1=x[0].value;
-//     player2=x[1].value;
-//    if(player1=="" || player2==""){
-//     alert("Please enter the name of both players");
-//     return;
-//    }
-//    location.href="home.html";
-//    alert("Welcome " + player1 + " and " + player2);
-//    })
-// ///
+let x=document.querySelectorAll(".player");
+let start=document.getElementById("start");
+ let player1,player2;
+start.addEventListener("click",function(){
+    player1=x[0].value;
+    player2=x[1].value;
+  if(player1=="" || player2==""){
+   alert("Please enter the name of both players");
+   return;
+  }
+   location.href="home.html";
+   alert("Welcome " + player1 + " and " + player2);
+   localStorage.setItem("player1", player1); // ← save before redirect
+    localStorage.setItem("player2", player2);
+    location.href = "home.html";
+  })
+player1 = localStorage.getItem("player1");
+player2 = localStorage.getItem("player2");
+document.getElementById("name").innerText = `${player1} vS ${player2}`;
 const winPatterns=[
     [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6] ];
 
